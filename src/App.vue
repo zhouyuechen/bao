56977e2a987c1073b6b102818b35658ff718167d
周玥辰 <zhouyuechen@163.com>
2019-03-15 18:25:54 +0800

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header-box  ></header-box>
+
+
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
+  import header from "./components/sub/Header";
+
+  export default {
+  name: 'App',
+    components: {//注册子组件
+      'header-box': header
+    },
 }
 </script>
 
-<style>
+<style scoped >
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/sub/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+      <p class="title">人才招聘网欢迎你~</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+ .header{
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   width: 100%;
+   height: 130px;
+   background-color: #f5f5f5;
+   .title{
+     font-size: 60px;
+   }
+ }
+</style>
